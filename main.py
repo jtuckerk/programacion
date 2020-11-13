02b3f28d3b6690c27efb45ec1f84da5f63efa40c
Tucker Kirven <tuckerkirven@gmail.com>
2020-11-13 12:54:04 -0800

    viernes 13

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import pygame
+from constansts import WIDTH, HEIGHT
+from Board import Board
+
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+
+def main():
+    
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                 pass
+        
+        board.draw_squares(WIN)
+        pygame.display.update()
+        
+    pygame.quit()
+main()

f21770622e83b78f7099db4cc0bc6e055569c590
Tucker Kirven <tuckerkirven@gmail.com>
2021-01-08 13:01:51 -0800

    errores

--- Board.py
+++ Board.py
@@ -6,7 +6,7 @@ import time
 
 class Board:
     def __init__(self, win):
-        self.board = []
+        self.datos_de_fichas = []
         self.selected_piece = None
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
@@ -15,58 +15,34 @@ class Board:
         
     def draw_squares(self):
         self.win.fill(BLACK)
-        time.sleep(1)
 
         for row in range(ROWS):
             # range(principio, fin, tamano_de_paso)
             for col in range(row % 2, ROWS, 2):
-                # un error en el contenido del tercer argumento "rect"
-                # las posiciones en el tuple tiene que estar multiplicados por el tamaño de los cuadros
-                # (r,g,b)
-                # r = np.random.randint(0,255)
-                # g = np.random.randint(0,255)
-                # b = np.random.randint(0,255)
-
-                # altura = np.random.randint(10,200)
-                # ancho = np.random.randint(10,200)
-                # pygame.draw.rect(surface, color, rect)
-                # rect = (x,y, altura, ancho)
                 pygame.draw.rect(self.win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                # colores lindos:
-                #pygame.draw.rect(self.win, (row*20, col*20, col*10+row*10), (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 
-    def create_board(self):
-       # pieza1 = Piece(row=0, col=1, color=RED)
-       # pieza1.draw(self.win)
-       # pieza2 = Piece(row=5, col=1, color=BLACK)
-       # pieza2.draw(self.win)
-       # pieza3 = Piece(row=0, col=3, color=RED)
-       # pieza3.draw(self.win)
-       # pieza4 = Piece(row=7, col=7, color=BLACK)
-       # pieza4.draw(self.win)
+    def crear_datos_de_fichas(self): # tenemos que cambiar este nombre
        
         for row in range(ROWS):
-            self.board.append([])
+            self.datos_de_fichas.append([])
             for col in range(COLS):
                 if col % 2 == ((row + 1) % 2):
                     if row < 3:
-                        self.board[row].append(Piece(row,col,WHITE))
+                        self.datos_de_fichas[row].append(Piece(row,col,WHITE))
                     elif row > 4:
-                        self.board[row].append(Piece(row,col,RED))
+                        self.datos_de_fichas[row].append(Piece(row,col,RED))
                     else:
-                        self.board[row].append(None)
+                        self.datos_de_fichas[row].append(None)
                 else:
-                    self.board[row].append(None)
+                    self.datos_de_fichas[row].append(None)
         print("cols y rows", COLS, ROWS)
-        print("Tamano del tablero", len(self.board), len(self.board[0]))
+        print("Tamano del tablero", len(self.datos_de_fichas), len(self.datos_de_fichas[0]))
     
     def draw(self):
         self.draw_squares()
-
-
         for row in range(ROWS):
             for col in range(COLS):
-                piece = self.board[row][col]
+                piece = self.datos_de_fichas[row][col]
                 if piece is not None:
                     piece.draw(self.win)
         
--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def main():
     
     run = True
     clock = pygame.time.Clock()
-    board = Board(WIN)
-    while run:
+    board = Board.(WIN) # A.
+    while run: #C
         clock.tick(FPS)
         
         for event in pygame.event.get():
@@ -22,11 +22,15 @@ def main():
                 
             if event.type == pygame.MOUSEBUTTONDOWN:
                  pass
+
+        # 1. variables de nuestra logico del juego - guardado en memoria
+        # 2. representacions de la ventana/pantalla de pygame - los variables dentro de pygame
+        # 3. lo que vimos en la pantalla (memoria de la pantalla)
         
-        board.draw_squares()
-        board.create_board()
-        board.draw()
-        pygame.display.update()
+        board.crear_datos_de_fichas() #B # iniciar nuestra representacion del tablero
+        board.draw() # poner los cambios del ventana del tablero en memoria
+
+        pygame.display.update() # mostramos los cambios en la pantalla
         
         
     pygame.quit()
